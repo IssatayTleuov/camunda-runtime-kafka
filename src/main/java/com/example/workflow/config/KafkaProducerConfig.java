a2faa9d43fc942dd082903e68bd8857a1ed80f80
ITleuov <Isatay.Tleuov@eubank.kz>
2023-03-14 09:43:54 +0600

    Updated application.yaml

--- src/main/java/com/example/workflow/config/KafkaProducerConfig.java
+++ src/main/java/com/example/workflow/config/KafkaProducerConfig.java
@@ -1,12 +1,14 @@
 package com.example.workflow.config;
 
 import com.example.workflow.dto.HistoryEventDto;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaProducerConfig {
